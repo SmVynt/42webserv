d08e8fde38a44d283084b16068590dc834bf266b
Vspaceman11 <pushkarov.vladyslav@gmail.com>
2026-02-09 17:48:01 +0100

    Init Cluster class

--- includes/Cluster.hpp
+++ includes/Cluster.hpp
@@ -1,9 +1,24 @@
 #pragma once
 #include <poll.h>
+#include <vector>
+#include <string>
+#include <map>
+#include "Config.hpp"
+
+class VirtualServer;
+
 class Cluster {
 	public:
 		Cluster();
+		Cluster(const std::vector<Config>& config);
+		Cluster(const Cluster& other);
+		Cluster& operator=(const Cluster& other);
 		~Cluster();
+
+		void	setupCluster();
+		void	run();
 	private:
-		int _pollFd;
+		std::vector<VirtualServer>	_servers;
+		std::vector<struct pollfd>	_pollfds;
+		std::map<int, int>			_listen_sockets;
 };
