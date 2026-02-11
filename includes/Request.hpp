55090fe59b65c0f079d2f34f04dc28a25174ac98
SuPuHe <mizin.a.s.2006@gmail.com>
2026-02-11 14:33:31 +0100

    create template of request parsing

--- includes/Request.hpp
+++ includes/Request.hpp
@@ -1,10 +1,30 @@
 #ifndef REQUEST_HPP
 # define REQUEST_HPP
 
+#include <string>
+#include <map>
+#include <vector>
+
 class Request {
-	public:
-		Request();
-		~Request();
+public:
+	enum State { METHOD_LINE, HEADERS, BODY, DONE, ERROR };
+
+private:
+	State		_state;
+	std::string	_raw_storage;
+
+	std::string	_method;
+	std::string	_path;
+	std::string	_http_version;
+	std::string	_body;
+	std::map<std::string, std::string>	_headers;
+
+public:
+	Request();
+
+	void consume(const std::string &new_chunk);
+
+	bool isFinished() const;
 };
 
 #endif
--- srcs/parser/Request.cpp
+++ srcs/parser/Request.cpp
@@ -1,5 +1,27 @@
 #include "Request.hpp"
 
-Request::Request() {}
+Request::Request(): _state(METHOD_LINE) {}
 
 Request::~Request() {}
+
+bool	Request::isFinished() const { return _state == DONE || _state == ERROR; }
+
+void	Request::consume(const std::string &new_chunk){
+	_raw_storage += new_chunk;
+
+	while (true){
+		if (_state == METHOD_LINE){
+			size_t pos = _raw_storage.find("\r\n");
+			if (pos == std::string::npos)
+				break;
+
+			std::string line = _raw_storage.substr(0, pos);
+			_raw_storage.erase(0, pos + 2);
+			//todo parse_line
+			_state = HEADERS;
+		}
+		else if (_state = HEADERS){
+			//todo parse_headers
+		}
+	}
+}
