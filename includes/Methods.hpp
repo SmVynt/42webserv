36b8c81511dd08941ea48203d446f684786ea3af
SuPuHe <mizin.a.s.2006@gmail.com>
2026-02-18 14:43:01 +0100

    implement get method. Still need to create method that will handle autoindex

---
+++ includes/Methods.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Response.hpp"
+#include "Request.hpp"
+#include "utils.hpp"
+#include <filesystem>
+#include <unistd.h>
+
+class RequestHandler{
+	private:
+		static Response			handleGet(const Request &req, const Location &loc);
+		static Response			handlePost(const Request &req, const Location &loc);
+		static Response			handleDelete(const Request &req, const Location &loc);
+
+		static std::string		getMimeType(const std::string &path);
+		static bool				isDirectory(const std::string &path);
+		static bool				fileExists(const std::string &path);
+		static const Location	*findLocation(const std::string &uri, const ServerConfig &config);
+
+	public:
+		static Response			handleRequest(Request &req, const ServerConfig &config);
+
+};
